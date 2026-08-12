538ac8c112b7cba970bdd5500aaaee4d8e28e2d0
soKrizpy <soKrizpy@github.com>
2026-08-12 14:24:58 +0700

    feat: enable smooth CSS transition when toggling dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="dark"
           enableSystem
-          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
